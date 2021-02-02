9493326adf78b598d1224fa7c3a3efb5b5fb8349
alexandreLamarre <alex7285@gmail.com>
2021-02-02 17:09:24 -0500

    Added main HTML router function

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
   "net/http"
   "github.com/alexandreLamarre/Quantum-Chess-Backend/pkg/websocket"
+  "github.com/alexandreLamarre/Quantum-Chess-Backend/pkg/quantum"
 )
 
 
@@ -16,6 +17,7 @@ func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
     }
 
     client := &websocket.Client{
+        ID: "john",
         Conn: conn,
         Pool: pool,
     }
@@ -24,9 +26,33 @@ func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
     client.Read()
 }
 
+func parseHTMLRoute(path string) (act string, uuid string, ok bool) {
+  return "", "", false
+}
+
+func doAction(action string, uuid string) bool{
+  return true
+}
+
+func RootServer(w http.ResponseWriter, r *http.Request){
+  if r.URL.Path == "/" {
+    // do nothing for now
+  } else if act, uuid, ok := parseHTMLRoute(r.URL.Path); ok{
+    // have a function handle different game room actions
+    res := doAction(act, uuid)
+    if !res{
+      http.Error(w, "Unexpected handler error", 400)
+    }
+  } else {
+    http.Error(w, "Not found", http.StatusNotFound)
+  }
+}
+
 func setupRoutes() {
     pool := websocket.NewPool()
     go pool.Start()
+
+    http.HandleFunc("/", RootServer)
 
     http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
         serveWs(pool, w, r)
@@ -34,7 +60,9 @@ func setupRoutes() {
 }
 
 func main() {
-    fmt.Println("Distributed Chat App v0.01")
+    fmt.Println("Quantum Chess App v0.01")
+    fmt.Println("Passed quantum tests?")
+    fmt.Println(quantum.TestAllQuantum())
     setupRoutes()
     http.ListenAndServe(":8080", nil)
 }
