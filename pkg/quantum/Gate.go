d67a35fd4f5664b06a68679329fb3521d78905be
alexandreLamarre <alex7285@gmail.com>
2021-02-03 15:06:33 -0500

    implemented Pauli Gates & finished setting up state methods/tests

--- pkg/quantum/Gate.go
+++ pkg/quantum/Gate.go
@@ -16,45 +16,127 @@ type Gate struct{
   matrix []complex128 //matrices are stored in 1d arrays ordered by rows.
 }
 
-func Hadamard(qbit_size int) Gate {
+func Hadamard(qbit_size int) (Gate, bool) {
   size := float64(qbit_size)
   var hadamard Gate;
-  var sqrt_constant = complex(math.Sqrt(2), 0)
-  hadamard.constant = sqrt_constant
+  if(qbit_size <= 0){
+    //#TODO: ERROR HANDLING
+    return hadamard, true
+  }
+  var constant = complex(1/math.Sqrt(2), 0)
   array_size := int(math.Pow(2, 2*size))
   hadamard.matrix = make([]complex128, array_size, array_size);
   basic_gate := [4]complex128{complex(1.0, 0.0), complex(1.0, 0.0),
                                 complex(1.0, 0.0), complex(-1.0, 0.0)}
   if(DEBUG_GATE){fmt.Println(len(hadamard.matrix))}
-  if qbit_size == 1 {
-    for i := 0; i < len(basic_gate); i++{
-      hadamard.matrix[i] = basic_gate[i]
-    }
-  } else{
-    var new_gate []complex128
-    var new_c complex128
-    for i:= 1; i < qbit_size; i++{
-      if(i == 1){
-        new_gate, new_c = tensorProduct(basic_gate[:], sqrt_constant, basic_gate[:], sqrt_constant)
-      } else{
-        new_gate, new_c = tensorProduct(new_gate, new_c, basic_gate[:], sqrt_constant)
-      }
-      if(DEBUG_GATE){ fmt.Println("tensor product",new_gate, new_c)}
-    }
-    hadamard.constant = new_c
 
-    for i:= 0; i < len(new_gate); i++{
-      hadamard.matrix[i] = new_gate[i]
-    }
+  c,gate,err := createGate(qbit_size, constant, basic_gate[:])
+  if err{
+    //#TODO ERROR HANDLING
   }
-  return hadamard
+  hadamard.constant = c
+  for i:= 0; i < len(gate); i++{
+    hadamard.matrix[i] = gate[i]
+  }
+
+  return hadamard, false
 }
 
+func PauliX(qbit_size int) (Gate,bool){
+  size := float64(qbit_size)
+  var paulix Gate;
+  if(qbit_size <= 0){
+    //#TODO: ERROR HANDLING
+    return paulix, true
+  }
+  var constant = complex(1, 0)
+  array_size := int(math.Pow(2, 2*size))
+  paulix.matrix = make([]complex128, array_size, array_size);
+  basic_gate := [4]complex128{complex(0.0, 0.0), complex(1.0, 0.0),
+                                complex(1.0, 0.0), complex(0.0, 0.0)}
+  if(DEBUG_GATE){fmt.Println(len(paulix.matrix))}
+
+  c,gate,err := createGate(qbit_size, constant, basic_gate[:])
+  if err{
+    //#TODO ERROR HANDLING
+  }
+  paulix.constant = c
+  for i:= 0; i < len(gate); i++{
+    paulix.matrix[i] = gate[i]
+  }
+
+  return paulix, false
+}
+
+func PauliY(qbit_size int) (Gate, bool){
+  size := float64(qbit_size)
+  var pauliy Gate;
+  if(qbit_size <= 0){
+    //#TODO: ERROR HANDLING
+    return pauliy, true
+  }
+  var constant = complex(1.0, 0.0)
+  array_size := int(math.Pow(2, 2*size))
+  pauliy.matrix = make([]complex128, array_size, array_size);
+  basic_gate := [4]complex128{complex(0.0, 0.0), complex(0.0, -1.0),
+                                complex(0.0, 1.0), complex(0.0, 0.0)}
+  if(DEBUG_GATE){fmt.Println(len(pauliy.matrix))}
+
+  c,gate,err := createGate(qbit_size, constant, basic_gate[:])
+  if err{
+    //#TODO ERROR HANDLING
+  }
+  pauliy.constant = c
+  for i:= 0; i < len(gate); i++{
+    pauliy.matrix[i] = gate[i]
+  }
+
+  return pauliy, false
+}
+
+func PauliZ(qbit_size int) (Gate, bool){
+  size := float64(qbit_size)
+  var pauliz Gate;
+  if(qbit_size <= 0){
+    //#TODO: ERROR HANDLING
+    return pauliz, true
+  }
+  var constant = complex(1.0, 0)
+  array_size := int(math.Pow(2, 2*size))
+  pauliz.matrix = make([]complex128, array_size, array_size);
+  basic_gate := [4]complex128{complex(1.0, 0.0), complex(0.0, 0.0),
+                                complex(0.0, 0.0), complex(-1.0, 0.0)}
+  if(DEBUG_GATE){fmt.Println(len(pauliz.matrix))}
+
+  c,gate,err := createGate(qbit_size, constant, basic_gate[:])
+  if err{
+    //#TODO ERROR HANDLING
+  }
+  pauliz.constant = c
+  for i:= 0; i < len(gate); i++{
+    pauliz.matrix[i] = gate[i]
+  }
+
+  return pauliz, false
+}
+
+func createGate(qbit_size int, c complex128, basic_gate []complex128) (complex128, []complex128, bool){
+    var new_gate []complex128 = basic_gate[:]
+    var new_c complex128 = c
+    var err bool
+    for i:= 1; i < qbit_size; i++{
+        new_c, new_gate, err = tensorProduct(new_gate, new_c, basic_gate[:], c)
+    }
+    return new_c, new_gate, err
+}
+
+
+
 func tensorProduct(A []complex128, c1 complex128,
-                    B []complex128, c2 complex128) (new_gate []complex128, new_c complex128){
+                    B []complex128, c2 complex128) (complex128, []complex128, bool){
   res:= make([]complex128, len(A)*len(B), len(A)*len(B))
   var c complex128
-  if(c1 == c2 && c1 == complex(math.Sqrt(2), 0)){
+  if(c1 == c2 && c1 == complex(1/math.Sqrt(2), 0)){
     c = complex(0.5, 0.0) //most common constants will be sqrt(2), so here we avoid
                             // at least some rounding errors
   } else{
@@ -81,5 +163,5 @@ func tensorProduct(A []complex128, c1 complex128,
     }
   }
 
-  return res, c
+  return c, res, false
 }
--- pkg/quantum/test.go
+++ pkg/quantum/test.go
@@ -2,31 +2,31 @@ package quantum
 
 import(
   "fmt"
-  "math"
-  "time"
 )
 
-var DEBUG = false
+var DEBUG = true
 
 func TestAllQuantum() bool {
   if(DEBUG){selectDebug()}
 
-  h := Hadamard(1)
-  h2 := Hadamard(2)
-  start := time.Now()
-  h3 := Hadamard(8)
-  duration := time.Since(start)
-  fmt.Println("time to execute Hadamard", duration)
-  fmt.Println(h)
-  fmt.Println(h2)
-  size:= int(math.Sqrt(float64(len(h3.matrix))))
-  fmt.Println("")
-  fmt.Println("")
-  for i:= 0; i < size; i++{
-    // fmt.Println(h3.matrix[i*size:(i+1)*size])
-  }
+  fmt.Println("==== Gates ====\n")
+  test:= testGates()
+  fmt.Println()
+  fmt.Println("Gates test sucessful?", test)
+  fmt.Println()
+
+  fmt.Println("==== States ==== \n")
+  test2 := testStates()
+  fmt.Println()
+  fmt.Println("States test successful?", test2)
+
+  fmt.Println("==== States n Gates ==== \n")
+  test3 := testGatesAndStates()
+  fmt.Println()
+  fmt.Println("States n Gates test successful?", test3)
+
   fmt.Println("Passed All quantum tests?")
-  return true
+  return test && test2
 }
 
 /**
@@ -36,5 +36,49 @@ depending on specific file we want to debug
 DEBUG_GATE: Gate.go
 **/
 func selectDebug(){
-  DEBUG_GATE = true
+  DEBUG_GATE = false
+  DEBUG_STATE = true
+}
+
+func testGates() bool{
+  h,err := Hadamard(1)
+  if(err){ return false}
+  h2, err := Hadamard(2)
+  if(err){ return false}
+  h3, err := Hadamard(3)
+  if(err){ return false}
+  fmt.Println(h)
+  fmt.Println(h2)
+  fmt.Println(h3)
+  px,err := PauliX(2)
+  if(err){return false}
+  fmt.Println()
+  fmt.Println(px)
+  return true
+}
+
+func testStates() bool{
+  s := MakeState(1)
+  fmt.Println(s)
+  var direk1 [2]complex128
+  direk1[0] = complex(1.0, 0.0)
+  direk1[1] = complex(0.0, 0.0)
+  s.SetState(direk1[:])
+  fmt.Println(s)
+  return true
+}
+
+func testGatesAndStates() bool {
+  s := MakeState(1)
+  var direk1 [2]complex128
+  direk1[0] = complex(1.0, 0.0)
+  direk1[1] = complex(0.0, 0.0)
+  s.SetState(direk1[:])
+  fmt.Println("state", s)
+  h, err:= Hadamard(1)
+  if(err){return false}
+  fmt.Println("gate", h)
+  s.ApplyGate(h)
+  fmt.Println("mixed state", s)
+  return true
 }
